567b5fe69ed43d1a5d9dfcf19fd51d401b5b1fd1
Neil Roberts <bpeeluk@yahoo.co.uk>
2013-03-29 15:11:30 +0000

    Clear color to white

--- chocojs.js
+++ chocojs.js
@@ -427,7 +427,7 @@ Choco.prototype.paint = function ()
 
   gl.useProgram (this.program);
 
-  gl.clearColor (0.0, 0.0, 0.0, 1.0);
+  gl.clearColor (1.0, 1.0, 1.0, 1.0);
   gl.clear (gl.COLOR_BUFFER_BIT);
 
   gl.bindBuffer (gl.ARRAY_BUFFER, this.chunkBuffer);
